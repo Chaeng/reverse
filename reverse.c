99178d5f4fe68c5740a8651be16ff910aaf920b8
Ploypaphat (Chaeng) Saltz <chaeng4@gmail.com>
2016-09-14 12:41:02 -0700

    My first draft on Sep 14, 2016

---
+++ reverse.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void string_reverse1(char *string)
+{
+    /* your code here */
+
+}
+// char *string_reverse2(const char *string)
+// {
+//      /* your code here */
+//     char *str_reverse = string;
+//     return *str_reverse;
+// }
+
+int main() {
+   /* File copying */
+   int c;
+
+   c = getchar();
+   while(c != EOF){
+   	//putchar(c);
+   	c = getchar();
+   }
+   return 0;
+}
+
